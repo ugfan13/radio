32d0129fa47c83e13f1d4a0e116091f217aaa9b9
Denis Sitek <ugfan13@gmail.com>
2021-01-04 19:20:41 +0900

    Initial commit

---
+++ src/main/java/ru/netology/domain/radio.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class radio {
+    int volume;
+    int MaxVolume = 10;
+    int MinVolume = 0;
+    int NumberStation;
+    int MinNumberStation = 0;
+    int MaxNumberStation = 9;
+    int getVolume;
+    int getNumberStation;
+
+
+}
---
+++ src/test/java/ru/netology/domain/radioTest.java
@@ -0,0 +1,7 @@
+package ru.netology.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class radioTest {
+
+}
